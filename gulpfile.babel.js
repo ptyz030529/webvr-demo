a8976ce57dd36165408a235818bcd64d85b1421f
Jimba <ptyz@example.com>
2016-05-21 01:30:04 +0800

    add test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,31 +5,35 @@ import babel       from "gulp-babel";
 import browserSync from "browser-sync";
 import gulp        from "gulp";
 import mocha       from "gulp-mocha";
+import plumber     from "gulp-plumber";
+import jsbeautifier from "gulp-jsbeautifier";
 
 let serve = browserSync.create();
-
 
 gulp.task("serve", ()=>{
   browserSync.init({
     server: "./"
   });
 
-  gulp.watch("./src/**/*.js", gulp.parallel("js")).on('change', browserSync.reload);
+  gulp.watch("./src/**/*.js", gulp.parallel("js", "test")).on('change', browserSync.reload);
   gulp.watch("./index.html").on('change', browserSync.reload);
 });
 
 
 gulp.task("js", ()=>{
   return gulp.src("src/**/*.js")
+  .pipe(plumber())
   .pipe(babel({
     presets: ['es2015'],
     plugins: ["external-helpers-2","transform-es2015-modules-umd"]
   }))
+  .pipe(jsbeautifier())
   .pipe(gulp.dest("dist"));
 });
 
 gulp.task("test", ()=>{
   return gulp.src("test/test.js")
+  .pipe(plumber())
   .pipe(mocha( { compilers: { js: babel } }));
 });
 
